6a57d633bb6a6a86c8fc78491124b53ed9fdbb8f
tubudan <huxiang0306@163.com>
2019-08-14 08:33:32 +0800

    first commit -tubudan

---
+++ src/main/pers/hx/wheels/time/ForwardAndBackwardNDays.java
@@ -0,0 +1,30 @@
+package main.pers.hx.wheels.time;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ForwardAndBackwardNDays {
+    public static void main(String[] args) {
+        int days = 10;
+        System.out.println("Hello World!");
+        ForwardAndBackwardNDays fabnd = new ForwardAndBackwardNDays();
+        fabnd.backWard(days);   //前第10天
+        fabnd.forwWard(days);   //后第10天
+    }
+
+    public void backWard(int n) {
+        Calendar calendar1 = Calendar.getInstance();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd");
+        calendar1.add(Calendar.DATE, -n);
+        String three_days_ago = sdf1.format(calendar1.getTime());
+        System.out.println(three_days_ago);
+    }
+
+    public void forwWard(int n) {
+        Calendar calendar1 = Calendar.getInstance();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd");
+        calendar1.add(Calendar.DATE, n);
+        String three_days_after = sdf1.format(calendar1.getTime());
+        System.out.println(three_days_after);
+    }
+}
